3b0c8316ec3a27f260ca01077df945f3881d7f24
Emin Mamedov <75082416+cytank@users.noreply.github.com>
2022-04-27 14:36:49 +0400

    Add files via upload

---
+++ js/lac.js
@@ -0,0 +1,71 @@
+/* programist */   let pr = {
+    "techer": "Samirə Mamedova",
+    "foto": "img/cisi.jpeg",
+    "ders": "Programist",
+    "vaxt": "16-00 : 17-00",
+    "gun": "Çərşənbə A, Cümə A, Şənbə"
+}
+/* sahmat */    let sr = {
+    "techer": "Davit Xizanişvili",
+    "foto": "img/cisi.jpeg",
+    "ders": "Şahmat",
+    "vaxt": "10-00 : 14-00 : 14-00",
+    "gun": "Bazar ərtəsi, Cümə A,  Cümə, Şənbə"
+}
+/* xalca */    let xl = {
+    "techer": "Nabat Anazarova",
+    "foto": "img/cisi.jpeg",
+    "ders": "Xalça",
+    "vaxt": "11-00",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+/* reqs */    let rq = {
+    "techer": "Keti Kopilaşvili",
+    "foto": "img/cisi.jpeg",
+    "ders": "Rəqs",
+    "vaxt": "14-45 : 16-15",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+/* ingilis */    let ing = {
+    "techer": "Aygün Mamedova",
+    "foto": "img/cisi.jpeg",
+    "ders": "İngilis Dili",
+    "vaxt": "14-00 : 15-00 : 16-00 :",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+/* mentiq */    let ment = {
+    "techer": "Abbas Abbasov",
+    "foto": "img/cisi.jpeg",
+    "ders": "Məntiq",
+    "vaxt": "16-00 : 17-00",
+    "gun": "Çərşənbə A, Cümə A, Şənbə"
+}
+/* piano */    let pi = {
+    "techer": "Rahilə Ataxəlilova",
+    "foto": "img/cisi.jpeg",
+    "ders": "Piano",
+    "vaxt": "11-00",
+    "gun": "Çərşənbə A, Cümə A, Şənbə"
+}
+/* resm */   let re = {
+    "techer": "Günel Zeynalova",
+    "foto": "img/cisi.jpeg",
+    "ders": "Rəsm",
+    "vaxt": "14-00 : 15-30",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+/* gurcu */    let gu = {
+    "techer": "Natela Şubatidze",
+    "foto": "img/cisi.jpeg",
+    "ders": "Gürcü",
+    "vaxt": "14-00 : 15-00 : 16-00 ",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+/* riyaziyyat */    let riy = {
+    "techer": "Günel Abdullayeva",
+    "foto": "img/cisi.jpeg",
+    "ders": "Riyaziyat",
+    "vaxt": "16-00 : 17-00",
+    "gun": "Bazar ərtəsi,Çərşənbə , Cümə "
+}
+
